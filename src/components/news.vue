c8dae64f3e42e9bb3d37b958a15747a389938448
Ajon <wychuqiang@163.com>
2017-04-21 22:31:31 +0800

    vuejs路由

---
+++ src/components/news.vue
@@ -0,0 +1,27 @@
+/**
+* Created by LinChuQiang on 2017/4/21.
+*/
+<template>
+	<div class="row">
+		<div class="news">
+			<div class="news-hader" v-for="news in newsList">
+				<h2>{{news.title}} <small>{{news.pubtime}}</small></h2>
+				<p>{{news.desc}}</p>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				newsList:[
+					{pubtime:"2016-10-29",title:"探索之路",desc:"是手机团队的探索之路"},
+					{pubtime:"2016-10-28",title:"系统之战",desc:"如何支持业务解决"},
+					{pubtime:"2016-10-27",title:"大文件存储",desc:"背后的你不得不知的技术"},
+					{pubtime:"2016-10-26",title:"飞天进化",desc:"阿里巴巴技术委员会"}
+				]
+			};
+		}
+	}
+</script>
--- src/jsSrc/index.js
+++ src/jsSrc/index.js
@@ -3,20 +3,28 @@
  */
 //会去node_modules\vue\types\
 import Vue from "vue";
-
+//通过 Browserify 或 Webpack 使用 CommonJS 兼容模块
+import VueRouter  from "vue-router";
 // 导入组件
-import about_me from "./../components/aboutme.vue";
-import userlogin from "./../components/user-login.vue";
+import pageNav from "./../components/page-nav.vue";
+import aboutMe from "./../components/aboutme.vue";
+import userLogin from "./../components/user-login.vue";
+import news from "./../components/news.vue";
 
-new Vue({
-	el:".container",
-	//Vue 实例的数据对象
-	data:{},
-	// 挂载组件,about_me就是组件名称
-	components:{
-		// <me_component> 将只在父模板可用
-		"me_component":about_me,
-		"user_login": userlogin
-	}
-	
-})
+// 全局使用插件
+Vue.use( VueRouter );
+const routes = [
+	{ path : '/news',component : news,name : 'news', },
+	{ path : '/login',component : userLogin,name : 'userLogin', },
+	{ path : '/about',component: aboutMe,name : 'aboutMe', },
+]
+const router = new VueRouter( {
+	routes
+} )
+//注册全局组件，使用 Vue.component(tagName, options)
+Vue.component("nav_component",pageNav);
+const myvue = new Vue( {
+	router:router
+} ).$mount( '.container' )
+
+
