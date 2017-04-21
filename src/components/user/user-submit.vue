0d199d41ba1bb806d2a70a778090aac9aedbc604
Ajon <wychuqiang@163.com>
2017-04-21 23:06:14 +0800

    ajax请求插件vue-resource的学习

--- src/components/user/user-submit.vue
+++ src/components/user/user-submit.vue
@@ -3,12 +3,12 @@
 */
 
 <template>
-	<div class="row">
-	<div class = "form-group">
-		<div class = "col-sm-10 col-sm-offset-2">
-			<button type = "button" class = "btn btn-default" @click = "test">提交</button>
+	<div class = "row">
+		<div class = "form-group">
+			<div class = "col-sm-10 col-sm-offset-2">
+				<button type = "button" class = "btn btn-default" @click = "test">提交</button>
+			</div>
 		</div>
-	</div>
 	</div>
 </template>
 
@@ -20,9 +20,34 @@
 				//在user-submit组件里，去访问user-name组件的data对象
 				//console.log(this.$parent.$refs.username.$data.username);
 				//拿到父组件定义的属性username，也就是user-name触发change事件发送过来的数据
-				console.log(this.$parent.$data.username);
-				console.log(this.$parent.$data.userpass);
-				console.log(this.$parent.$data.userarea);
+				console.log( this.$parent.$data.username )
+				console.log( this.$parent.$data.userpass )
+				console.log( this.$parent.$data.userarea )
+				this.$http.get( "../test.php" ).then( response => {
+						console.log( response );
+					},response => {
+						// error callback
+						console.log( response );
+					}
+				)
+				this.$http.get( '../image.png' ).then( response => {
+					// resolve to Blob
+					console.log( response.blob() );
+				} ).then( blob => {
+					// use image Blob
+				} )
+				this.$http.post( '../test.php',{ foo : 'bar' } ).then( response => {
+					// get status
+					response.status;
+					// get status text
+					response.statusText;
+					// get 'Expires' header
+					response.headers.get( 'Expires' );
+					// get body data
+					console.log( response );
+				},response => {
+					console.log( response );
+				} );
 			}
 		}
 	}
--- src/jsSrc/index.js
+++ src/jsSrc/index.js
@@ -5,26 +5,27 @@
 import Vue from "vue";
 //通过 Browserify 或 Webpack 使用 CommonJS 兼容模块
 import VueRouter  from "vue-router";
+import VueResource from "vue-resource";
 // 导入组件
 import pageNav from "./../components/page-nav.vue";
 import aboutMe from "./../components/aboutme.vue";
 import userLogin from "./../components/user-login.vue";
 import news from "./../components/news.vue";
-
 // 全局使用插件
 Vue.use( VueRouter );
+Vue.use( VueResource );
 const routes = [
 	{ path : '/news',component : news,name : 'news', },
 	{ path : '/login',component : userLogin,name : 'userLogin', },
-	{ path : '/about',component: aboutMe,name : 'aboutMe', },
+	{ path : '/about',component : aboutMe,name : 'aboutMe', },
 ]
 const router = new VueRouter( {
 	routes
 } )
 //注册全局组件，使用 Vue.component(tagName, options)
-Vue.component("nav_component",pageNav);
+Vue.component( "nav_component",pageNav );
 const myvue = new Vue( {
-	router:router
+	router : router
 } ).$mount( '.container' )
 
 
