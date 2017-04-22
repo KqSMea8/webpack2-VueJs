59836b81681fa1581f4d27903ed17198cf37a6ee
Ajon <wychuqiang@163.com>
2017-04-22 17:00:25 +0800

    手动挂载子组件模板-1

--- src/components/user/user-name.vue
+++ src/components/user/user-name.vue
@@ -10,7 +10,6 @@
 				<!--:username 在input上绑定一个username属性 赋值为 组件的data.username -->
 				<!--触发 input 事件时，将该元素的值，更新到 组件的data.username-->
 				<input type = "text" class = "form-control" :placeholder = "placeholder" :username = "username" @input = "username=$event.target.value" @change = "userNameChg" @blur = "userNameVaild" v-uname:foo.a.b = "username" v-focus>
-				<label class = "label label-danger" v-if = "showErrorLabel">用户不合法</label>
 			</div>
 		</div>
 	</div>
@@ -21,8 +20,7 @@
 		props      : [ "placeholder" ],
 		data       : function() {
 			return {
-				username       : '',
-				showErrorLabel : false
+				username       : ''
 			};
 		},
 		methods    : {
@@ -30,14 +28,6 @@
 			userNameChg(){
 				// 调用父组件的方法
 				this.$emit( "iptChange","username",this.username )
-			},
-			userNameVaild(){
-				// 用户名改变的方法里判断 用户名是否复合要求
-				if( this.checkUserName( this.username ) ) {
-					this.showErrorLabel = false; // 如果验证没有通过就显示错误提示
-				} else {
-					this.showErrorLabel = true;
-				}
 			}
 		},
 		mounted(){}
