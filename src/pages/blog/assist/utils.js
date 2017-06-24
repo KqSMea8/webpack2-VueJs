76820df811ab95c7d9dcf67efa50bbe7c5b3bc50
Ajon <wychuqiang@163.com>
2017-06-24 23:58:29 +0800

    MPA+WEBPACK2+VUE2

--- src/pages/blog/assist/utils.js
+++ src/pages/blog/assist/utils.js
@@ -1,4 +1,4 @@
-// 将对象o转换为字符串，再从中解析出JSON对象
+// TODO-es6：将对象o转换为字符串，再从中解析出JSON对象
 // see http://blog.csdn.net/wangxiaohu__/article/details/7254598
 export function clone (o) {
   return JSON.parse(JSON.stringify(o))
